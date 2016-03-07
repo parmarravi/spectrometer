0e364429cc3a6e694146b170f4f6b5d506016aa8
parmarravi950 <parmarravi950@gmail.com>
2016-03-07 22:01:01 +0530

    upgrade to home page and gallery  and also  added capture function

--- app.py
+++ app.py
@@ -1,44 +1,50 @@
-
-
 from flask import Flask, render_template
-import datetime
+from picamera import PiCamera
+from glob import glob
+from datetime import datetime
+import os
+
 app=Flask(__name__) #load flask module into python
+path = os.path.dirname(os.path.realpath(__file__))
+
+def get_photos():
+    photo_files = glob("%s/static/photos/*.jpg" % path)
+    photos = ["/static/photos/%s" % photo.split('/')[-1] for photo in photo_files]
+    return sorted(photos, reverse=True)
 
 @app.route('/')
-
 def index():
-    now =datetime.datetime.now()
-    timeString=now.strftime("%Y-%m-%d %H:%M:%S")
-    templateData = {
-        'title':'UVIspectralbench',
-        'time':timeString
-        }
-    return render_template('index.html',**templateData)
+    return render_template('index.html')
 
 @app.route('/capturespectra.html')
-
-def capturespectra():
-
+def capture():
+    timestamp = datetime.now().isoformat()
+    photo_path = '%s/static/photos/%s.jpg' % (path, timestamp)
+    with PiCamera() as camera:
+        camera.hflip = camera.vflip = True
+        camera.resolution = (640, 400)
+        camera.capture(photo_path)
     return render_template('capturespectra.html')
 
+
 @app.route('/absorption.html')
-
 def absorption():
-
+    
     return render_template('absorption.html')
 
 @app.route('/gallery.html')
 
 def gallery():
-
-    return render_template('gallery.html')
+    photos = get_photos()
+    return render_template('gallery.html',photos=photos)
 
 @app.route('/about.html')
-
 def about():
-
     return render_template('about.html')
 
+@app.route('/view/<photo>/')
+def view(photo):
+    return render_template('view.html', photo=photo)
 
 
 if __name__ =='__main__':
